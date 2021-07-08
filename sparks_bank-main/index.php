85de56c854e3d64c51be4d79e6c063b42640139d
Asra25 <85837380+Asra25@users.noreply.github.com>
2021-07-08 20:16:11 +0530

    Add files via upload

---
+++ sparks_bank-main/index.php
@@ -0,0 +1,22 @@
+<?php include('nav.php') ?>
+
+
+<font face=Adobe Gothic Std"font size="6"font color="black"><b><marquee><h1>Welcome To Union Bank</h1></marquee></b></font>
+    <div class="center">
+      <table cellpadding="20">
+        <tr cellpadding="20">
+          <th><a href="transfer_money.php"><button class="btn" id="green"><h3>View Customers</h3></button></th>
+          <th><a href="transfer_money.php"><button class="btn" id="green"><h3>Trasnfer Money</h3></button></th>
+          <th><a href="transactionhistory.php"><button class="btn" id="green"><h3>Transaction History</h3></button></th>
+        </tr>
+    </table> 
+</div>
+<body background="https://www.moneycrashers.com/wp-content/uploads/2019/09/money-transfer-international-from-laptop-remote-1068x713.jpg">
+
+</body>
+
+
+
+  
+
+</html>
