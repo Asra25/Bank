14567eb4247c6b5cc519d825c92b5da0b61e5708
Asra25 <85837380+Asra25@users.noreply.github.com>
2022-02-20 21:41:17 +0530

    Update transfer_money.php

--- sparks_bank-main/transfer_money.php
+++ sparks_bank-main/transfer_money.php
@@ -6,20 +6,18 @@
 
 <?php include('nav.php'); ?>
 
-<div class="container">
-<font face=Adobe Gothic Std"font size="5"font color="red"><b><marquee><h1>Transfer Money</h1></marquee></b></font>
+<marquee><h1>Transfer Money</h1></marquee></font>
         <br>
             <div class="row">
                 <div class="col">
-                    <div class="table-responsive-sm">
-                    <table class="table table-hover table-sm table-striped table-condensed table-bordered">
+                    <table>
                         <thead>
                             <tr>
-                            <th scope="col" class="text-center py-2">Id</th>
-                            <th scope="col" class="text-center py-2">Name</th>
-                            <th scope="col" class="text-center py-2">E-Mail</th>
-                            <th scope="col" class="text-center py-2">Balance</th>
-                            <th scope="col" class="text-center py-2">Operation</th>
+                            <th >Id</th>
+                            <th>Name</th>
+                            <th>E-Mail</th>
+                            <th>Balance</th>
+                            <th>Operation</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -27,10 +25,10 @@
                     while($rows=mysqli_fetch_assoc($result)){
                 ?>
                     <tr>
-                        <td class="py-2"><?php echo $rows['id'] ?></td>
-                        <td class="py-2"><?php echo $rows['name']?></td>
-                        <td class="py-2"><?php echo $rows['email']?></td>
-                        <td class="py-2"><?php echo $rows['balance']?></td>
+                        <td><?php echo $rows['id'] ?></td>
+                        <td><?php echo $rows['name']?></td>
+                        <td><?php echo $rows['email']?></td>
+                        <td><?php echo $rows['balance']?></td>
                         <td><a href="selectuserdetail.php?id= <?php echo $rows['id'] ;?>"> <button type="button" class="btn btn-success">Transact</button></a></td> 
                     </tr>
                 <?php
@@ -43,8 +41,6 @@
                 </div>
             </div> 
          </div>
-         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
-
+   
 </body>
 </html>
